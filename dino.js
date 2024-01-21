27eecb4e6407c44eab35c687e7e6391d2784c3ef
SaucyTuna <genczarlawrenz@gmail.com>
2024-01-21 16:31:37 +0800

    .

--- dino.js
+++ dino.js
@@ -1,11 +1,8 @@
-
-//board
 let board;
 let boardWidth = 750;
 let boardHeight = 250;
 let context;
 
-//dino
 let dinoWidth = 88;
 let dinoHeight = 94;
 let dinoX = 50;
@@ -19,7 +16,6 @@ let dino = {
     height : dinoHeight
 }
 
-//cactus
 let cactusArray = [];
 
 let cactus1Width = 34;
@@ -34,8 +30,7 @@ let cactus1Img;
 let cactus2Img;
 let cactus3Img;
 
-//physics
-let velocityX = -5; //cactus moving left speed
+let velocityX = -5; 
 let velocityY = 0;
 let gravity = .3;
 
@@ -47,11 +42,7 @@ window.onload = function() {
     board.height = boardHeight;
     board.width = boardWidth;
 
-    context = board.getContext("2d"); //used for drawing on the board
-
-    //draw initial dinosaur
-    // context.fillStyle="green";
-    // context.fillRect(dino.x, dino.y, dino.width, dino.height);
+    context = board.getContext("2d"); 
 
     dinoImg = new Image();
     dinoImg.src = "./img/dino.png";
@@ -69,7 +60,7 @@ window.onload = function() {
     cactus3Img.src = "./img/cactus3.png";
 
     requestAnimationFrame(update);
-    setInterval(placeCactus, 1000); //1000 milliseconds = 1 second
+    setInterval(placeCactus, 1000); 
     document.addEventListener("keydown", moveDino);
 }
 
@@ -80,12 +71,10 @@ function update() {
     }
     context.clearRect(0, 0, board.width, board.height);
 
-    //dino
     velocityY += gravity;
-    dino.y = Math.min(dino.y + velocityY, dinoY); //apply gravity to current dino.y, making sure it doesn't exceed the ground
+    dino.y = Math.min(dino.y + velocityY, dinoY); 
     context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
 
-    //cactus
     for (let i = 0; i < cactusArray.length; i++) {
         let cactus = cactusArray[i];
         cactus.x += velocityX;
@@ -100,7 +89,6 @@ function update() {
         }
     }
 
-    //score
     context.fillStyle="black";
     context.font="20px courier";
     score++;
@@ -113,12 +101,10 @@ function moveDino(e) {
     }
 
     if ((e.code == "Space" || e.code == "ArrowUp") && dino.y == dinoY) {
-        //jump
         velocityY = -10;
         e.preventDefault()
     }
     else if (e.code == "ArrowDown" && dino.y == dinoY) {
-        //duck
         e.preventDefault()
     }
 
@@ -129,7 +115,6 @@ function placeCactus() {
         return;
     }
 
-    //place cactus
     let cactus = {
         img : null,
         x : cactusX,
@@ -138,32 +123,32 @@ function placeCactus() {
         height: cactusHeight
     }
 
-    let placeCactusChance = Math.random(); //0 - 0.9999...
+    let placeCactusChance = Math.random(); 
 
-    if (placeCactusChance > .90) { //10% you get cactus3
+    if (placeCactusChance > .90) { 
         cactus.img = cactus3Img;
         cactus.width = cactus3Width;
         cactusArray.push(cactus);
     }
-    else if (placeCactusChance > .70) { //30% you get cactus2
+    else if (placeCactusChance > .70) { 
         cactus.img = cactus2Img;
         cactus.width = cactus2Width;
         cactusArray.push(cactus);
     }
-    else if (placeCactusChance > .50) { //50% you get cactus1
+    else if (placeCactusChance > .50) { 
         cactus.img = cactus1Img;
         cactus.width = cactus1Width;
         cactusArray.push(cactus);
     }
 
     if (cactusArray.length > 5) {
-        cactusArray.shift(); //remove the first element from the array so that the array doesn't constantly grow
+        cactusArray.shift(); 
     }
 }
 
 function detectCollision(a, b) {
-    return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
-           a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
-           a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
-           a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+    return a.x < b.x + b.width &&   
+           a.x + a.width > b.x &&   
+           a.y < b.y + b.height &&  
+           a.y + a.height > b.y;    
 }
--- flappybird.js
+++ flappybird.js
@@ -1,12 +1,10 @@
 
-//board
 let board;
 let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
-//bird
-let birdWidth = 34; //width/height ratio = 408/228 = 17/12
+let birdWidth = 34;
 let birdHeight = 24;
 let birdX = boardWidth/8;
 let birdY = boardHeight/2;
@@ -19,9 +17,9 @@ let bird = {
     height : birdHeight
 }
 
-//pipes
+
 let pipeArray = [];
-let pipeWidth = 64; //width/height ratio = 384/3072 = 1/8
+let pipeWidth = 64; 
 let pipeHeight = 512;
 let pipeX = boardWidth;
 let pipeY = 0;
@@ -29,9 +27,9 @@ let pipeY = 0;
 let topPipeImg;
 let bottomPipeImg;
 
-//physics
-let velocityX = -2; //pipes moving left speed
-let velocityY = 0; //bird jump speed
+
+let velocityX = -2; 
+let velocityY = 0; 
 let gravity = 0.2;
 
 let gameOver = false;
@@ -41,13 +39,8 @@ window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
-    context = board.getContext("2d"); //used for drawing on the board
+    context = board.getContext("2d"); 
 
-    //draw flappy bird
-    // context.fillStyle = "green";
-    // context.fillRect(bird.x, bird.y, bird.width, bird.height);
-
-    //load images
     birdImg = new Image();
     birdImg.src = "./flappybird.png";
     birdImg.onload = function() {
@@ -61,7 +54,7 @@ window.onload = function() {
     bottomPipeImg.src = "./bottompipe.png";
 
     requestAnimationFrame(update);
-    setInterval(placePipes, 1500); //every 1.5 seconds
+    setInterval(placePipes, 1500); 
     document.addEventListener("keydown", moveBird);
 }
 
@@ -72,24 +65,21 @@ function update() {
     }
     context.clearRect(0, 0, board.width, board.height);
 
-    //bird
     velocityY += gravity;
-    // bird.y += velocityY;
-    bird.y = Math.max(bird.y + velocityY, 0); //apply gravity to current bird.y, limit the bird.y to top of the canvas
+    bird.y = Math.max(bird.y + velocityY, 0); 
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
 
     if (bird.y > board.height) {
         gameOver = true;
     }
 
-    //pipes
     for (let i = 0; i < pipeArray.length; i++) {
         let pipe = pipeArray[i];
         pipe.x += velocityX;
         context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
 
         if (!pipe.passed && bird.x > pipe.x + pipe.width) {
-            score += 0.5; //0.5 because there are 2 pipes! so 0.5*2 = 1, 1 for each set of pipes
+            score += 0.5; 
             pipe.passed = true;
         }
 
@@ -98,12 +88,10 @@ function update() {
         }
     }
 
-    //clear pipes
     while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
-        pipeArray.shift(); //removes first element from the array
+        pipeArray.shift(); 
     }
 
-    //score
     context.fillStyle = "white";
     context.font="45px sans-serif";
     context.fillText(score, 5, 45);
@@ -118,9 +106,6 @@ function placePipes() {
         return;
     }
 
-    //(0-1) * pipeHeight/2.
-    // 0 -> -128 (pipeHeight/4)
-    // 1 -> -128 - 256 (pipeHeight/4 - pipeHeight/2) = -3/4 pipeHeight
     let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2);
     let openingSpace = board.height/4;
 
@@ -147,10 +132,8 @@ function placePipes() {
 
 function moveBird(e) {
     if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
-        //jump
         velocityY = -6;
 
-        //reset game
         if (gameOver) {
             bird.y = birdY;
             pipeArray = [];
@@ -163,8 +146,8 @@ function moveBird(e) {
 }
 
 function detectCollision(a, b) {
-    return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
-           a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
-           a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
-           a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+    return a.x < b.x + b.width &&   
+           a.x + a.width > b.x &&   
+           a.y < b.y + b.height &&  
+           a.y + a.height > b.y;    
 }
